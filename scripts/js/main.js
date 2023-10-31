d18af75c8a97647bbeb8e14edc4e2d4282e29c1f
Ichinose0 <148521661+Ichinose0@users.noreply.github.com>
2023-10-31 14:13:36 +0900

    Update

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,7 +1,17 @@
 function subscribe() {
-    alert("絶対しろよ");
-  }
+  alert("絶対しろよ");
+}
+
+function photos() {
+  alert("そんなものあるわけねえよなぁ");
+}
+
+function calc() {
+  let left_box = document.getElementById("left");
+  let right_box = document.getElementById("right");
+  let left = parseInt(left_box.value);
+  let right = parseInt(right_box.value);
   
-  function photos() {
-    alert("そんなものあるわけねえよなぁ");
-  }
+  let answer = document.getElementById("answer");
+  answer.innerText = left+right;
+}
